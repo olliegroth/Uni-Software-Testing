f882219edcb2a74deb5f8c8c39a8ac5133e99957
olliegroth <olliegroth2004@gmail.com>
2024-02-15 10:50:33 +0000

    Add unit tests to tdd directory

--- testDrivenDevelopment/busUnitTests/test_bus_ticket.py
+++ testDrivenDevelopment/busUnitTests/test_bus_ticket.py
@@ -4,3 +4,18 @@ def test_normal_ticket():
     normalPerson = Person(23, False)
     normalPerson.checkTicket()
     assert normalPerson.checkTicket() == 4
+
+def test_under_three_ticket():
+    normalPerson = Person(2, False)
+    normalPerson.checkTicket()
+    assert normalPerson.checkTicket() == 0
+
+def test_elderly_ticket():
+    normalPerson = Person(97, False)
+    normalPerson.checkTicket()
+    assert normalPerson.checkTicket() == 0
+
+def test_student_ticket():
+    normalPerson = Person(17, True)
+    normalPerson.checkTicket()
+    assert normalPerson.checkTicket() == 2
--- testDrivenDevelopment/person.py
+++ testDrivenDevelopment/person.py
@@ -10,7 +10,7 @@ class Person:
         if self.age < 3 or self.age >= 65:
             ticketCost = 0
 
-        if self.age < 19:
+        if self.studentStatus:
             ticketCost = 2
 
         return ticketCost
